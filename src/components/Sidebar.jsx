d6057159dcfb5bb49ce47b026fdc602a774d75ce
alcantara2509 <alcantara2509@gmail.com>
2020-11-24 12:40:04 -0300

    sidebar actived link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,47 +1,51 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Sidebar.css';
 
 function Sidebar() {
   return (
     <aside className="sidebar-container">
       <section className="logo-sidebar-container">
-        <Link to="/">
-          oi
+        <NavLink to="/">
           <img className="logo-sidebar" src="#" alt="Seiren Comics Logo" />
-        </Link>
+        </NavLink>
       </section>
       <section className="sidebar-menu-container">
-        <Link
+        <NavLink
           className="sidebar-links"
           to="/profile"
+          activeClassName="selected-link"
         >
           Profile
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           className="sidebar-links"
           to="/estante"
+          activeClassName="selected-link"
         >
           Estante
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           className="sidebar-links"
           to="/calendario"
+          activeClassName="selected-link"
         >
           Calendário
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           className="sidebar-links"
           to="/categorias"
+          activeClassName="selected-link"
         >
           Categorias
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           className="sidebar-links"
           to="/favoritos"
+          activeClassName="selected-link"
         >
           Favoritos
-        </Link>
+        </NavLink>
       </section>
       <section className="social-links">
         <a rel="noreferrer" href="https://www.instagram.com/" target="_blank">Insta</a>
