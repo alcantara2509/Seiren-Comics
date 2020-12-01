bba335623f2b7e13ebf239467a13205a88167c5b
alcantara2509 <alcantara2509@gmail.com>
2020-12-01 15:44:09 -0300

    carousel

--- src/pages/Shelf.jsx
+++ src/pages/Shelf.jsx
@@ -1,12 +1,20 @@
+/* eslint-disable no-magic-numbers */
 import React, { useEffect, useState } from 'react';
+// import { HashLink as Link } from 'react-router-hash-link';
 import Anchor from '../components/Anchor';
 import Sidebar from '../components/Sidebar';
 import Topbar from '../components/Topbar';
 import Banner from '../images/shelf-banner.png';
 import './Shelf.css';
+// import Highligths from '../components/Highligths';
 
 function Shelf() {
   const [apiResponseState, setApiResponseState] = useState([]);
+  const [numberCardNext, setNumberCardNext] = useState(5);
+  const [numberCardInicial, setNumberCardInicial] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [isDisabledPrev, setIsDisabledPrev] = useState(true);
+  const [max, setMax] = useState(0);
   const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
   useEffect(() => {
@@ -14,12 +22,34 @@ function Shelf() {
       const apiRequest = await fetch(url);
       const apiResponse = await apiRequest.json();
       setApiResponseState(apiResponse.meals);
+      setMax(apiResponse.length);
     };
 
     fetchUrl();
   }, []);
 
-  console.log(apiResponseState);
+  const handleClickNext = () => {
+    if (apiResponseState.length - 1 === numberCardNext) {
+      setIsDisabled(true);
+    }
+    setNumberCardNext(numberCardNext + 1);
+    setNumberCardInicial(numberCardInicial + 1);
+    setIsDisabledPrev(false);
+  };
+
+  const handleClickPrev = () => {
+    if (apiResponseState.length - 1 !== numberCardNext) {
+      setIsDisabled(false);
+    }
+    if (numberCardInicial === 1) {
+      setIsDisabledPrev(true);
+    }
+    setNumberCardNext(numberCardNext - 1);
+    setNumberCardInicial(numberCardInicial - 1);
+  };
+
+  console.log(max, numberCardNext);
+
   return (
     <section className="shelf-container">
       <Sidebar />
@@ -27,24 +57,138 @@ function Shelf() {
         <Topbar />
         <img src={ Banner } alt="banner" id="shelf-banner" />
         <Anchor />
-        <div className="categories-list">
+        {/* <Highligths api={ apiResponseState } /> */}
+        <div className="highligths-list">
           <h2 className="shelf-h2">Destaques</h2>
           <div className="tales-container">
             {
-              apiResponseState.map((tales, id) => (
-                <div
-                  key={ id }
-                  className="tales-card"
-                  style={ { backgroundImage: `url(${tales.strMealThumb})` } }
-                >
-                  <p className="naoentendir">{tales.strMeal}</p>
-                </div>
-              ))
+              apiResponseState.slice(numberCardInicial, numberCardNext)
+                .map((tales, id) => (
+                  <div
+                    key={ id }
+                    className="tales-card"
+                    style={ { backgroundImage: `url(${tales.strMealThumb})` } }
+                  >
+                    <p className="naoentendir">{tales.strMeal}</p>
+                  </div>
+                ))
             }
           </div>
+          <button
+            type="button"
+            onClick={ handleClickPrev }
+            disabled={ isDisabledPrev }
+          >
+            anterior
+
+          </button>
+          <button
+            type="button"
+            onClick={ handleClickNext }
+            disabled={ isDisabled }
+          >
+            proximo
+
+          </button>
+        </div>
+        <div className="highligths-list">
           <h2 className="shelf-h2">Continue Lendo</h2>
-          <h2 className="shelf-h2">Próximos Lançamentos</h2>
+          <div className="tales-container">
+            {
+              apiResponseState.slice(numberCardInicial, numberCardNext)
+                .map((tales, id) => (
+                  <div
+                    key={ id }
+                    className="tales-card"
+                    style={ { backgroundImage: `url(${tales.strMealThumb})` } }
+                  >
+                    <p className="naoentendir">{tales.strMeal}</p>
+                  </div>
+                ))
+            }
+          </div>
+          <button
+            type="button"
+            onClick={ handleClickPrev }
+            disabled={ isDisabledPrev }
+          >
+            anterior
+
+          </button>
+          <button
+            type="button"
+            onClick={ handleClickNext }
+            disabled={ isDisabled }
+          >
+            proximo
+
+          </button>
+        </div>
+        <div className="highligths-list">
+          <h2 className="shelf-h2">Próximos Lançamento</h2>
+          <div className="tales-container">
+            {
+              apiResponseState.slice(numberCardInicial, numberCardNext)
+                .map((tales, id) => (
+                  <div
+                    key={ id }
+                    className="tales-card"
+                    style={ { backgroundImage: `url(${tales.strMealThumb})` } }
+                  >
+                    <p className="naoentendir">{tales.strMeal}</p>
+                  </div>
+                ))
+            }
+          </div>
+          <button
+            type="button"
+            onClick={ handleClickPrev }
+            disabled={ isDisabledPrev }
+          >
+            anterior
+
+          </button>
+          <button
+            type="button"
+            onClick={ handleClickNext }
+            disabled={ isDisabled }
+          >
+            proximo
+
+          </button>
+        </div>
+        <div className="highligths-list">
           <h2 className="shelf-h2">Séries</h2>
+          <div className="tales-container">
+            {
+              apiResponseState.slice(numberCardInicial, numberCardNext)
+                .map((tales, id) => (
+                  <div
+                    key={ id }
+                    className="tales-card"
+                    style={ { backgroundImage: `url(${tales.strMealThumb})` } }
+                  >
+                    <p className="naoentendir">{tales.strMeal}</p>
+                  </div>
+                ))
+            }
+          </div>
+          <button
+            type="button"
+            onClick={ handleClickPrev }
+            disabled={ isDisabledPrev }
+          >
+            anterior
+
+          </button>
+          <button
+            type="button"
+            onClick={ handleClickNext }
+            disabled={ isDisabled }
+          >
+            proximo
+
+          </button>
         </div>
       </section>
     </section>
