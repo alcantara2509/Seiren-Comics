dfcdfcd766c0c40057ff649365c03772950d0dec
alcantara2509 <alcantara2509@gmail.com>
2020-12-03 20:17:43 -0300

    seiren comunity

--- src/pages/Shelf.jsx
+++ src/pages/Shelf.jsx
@@ -23,6 +23,17 @@ function Shelf() {
         <KeepReading />
         <Releases />
         <Series />
+        <h3 className="seiren-comunity-h3">Comunidade Seiren</h3>
+        <section className="seiren-comunity">
+          <section className="publicated-pages">
+            <h5 className="seiren-comunity-h5">Páginas Publicadas</h5>
+            <h1 className="seiren-comunity-h1">57345</h1>
+          </section>
+          <section className="online-users">
+            <h5 className="seiren-comunity-h5">Usuários Online</h5>
+            <h1 className="seiren-comunity-h1">345</h1>
+          </section>
+        </section>
       </section>
     </section>
   );
