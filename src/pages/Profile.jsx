95bf71207ded5c368c23219b4daa01f0534b8da5
alcantara2509 <alcantara2509@gmail.com>
2020-12-05 19:56:48 -0300

    css profile sub

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,6 +5,10 @@ import './Profile.css';
 
 function Profile() {
   const avatarSrc = 'https://blog.nebrass.fr/wp-content/uploads/Homer-Simpson-4-200x200.jpg';
+  const inds = ['name@gmail.com', 'name@gmail.com', 'name@gmail.com',
+    'name@gmail.com', 'name@gmail.com', 'name@gmail.com',
+    'name@gmail.com', 'name@gmail.com', 'name@gmail.com',
+    'name@gmail.com', 'name@gmail.com', 'name@gmail.com'];
 
   return (
     <section className="profile-container">
@@ -21,24 +25,47 @@ function Profile() {
             <div className="user-name-container">
               <h4>Nome de Usuário</h4>
               <input type="text" className="profile-input" />
-              <button type="button">Alterar Nome</button>
-              <div>
-                <p> Está com algum problema?</p>
-                <Link to="/">Contate o suporte</Link>
+              <div className="profile-btn-container">
+                <button type="button" className="profile-btn">Alterar Nome</button>
+              </div>
+              <div className="supp-contact">
+                <p className="supp-line"> Está com algum problema?</p>
+                <Link to="/" className="supp-line" id="supp-link">Contate o suporte</Link>
               </div>
             </div>
             <div className="password-container">
               <h4>Senha atual</h4>
-              <input type="text" className="profile-input" />
+              <input type="password" className="profile-input" />
               <h4>Nova senha</h4>
-              <input type="text" className="profile-input" />
-              <button type="button">Trocar Senha</button>
+              <input type="password" className="profile-input" />
+              <div className="profile-btn-container">
+                <button type="button" className="profile-btn">Trocar Senha</button>
+              </div>
             </div>
             <div className="ind-container">
               <h4>Suas Indicações</h4>
+              <div className="ind-wrap">
+                {
+                  inds.map((ind, id) => <li key={ id } className="inds">{ind}</li>)
+                }
+              </div>
             </div>
-            <div className="assinatura-container">
+            <div className="sub-container">
               <h4>Sua assinatura</h4>
+              <div className="sub-itens">
+                <div className="plan-conteiner">
+                  <p className="sub-p">Standart</p>
+                  <div className="value-container">
+                    <h5 id="sub-h5">$</h5>
+                    <h1 id="sub-h1">39.90</h1>
+                  </div>
+                </div>
+                <div>
+                  <p className="sub-p">mudar plano</p>
+                  <p className="sub-p" id="mid-p">suspender plano</p>
+                  <p className="sub-p" id="cancel-p">cancelar plano</p>
+                </div>
+              </div>
             </div>
           </section>
         </div>
