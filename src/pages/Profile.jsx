be6b6307ccd6ae245e70163ce45551eb9e664f70
alcantara2509 <alcantara2509@gmail.com>
2020-12-05 17:35:44 -0300

    css profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Favorites, KeepReading, Sidebar, Topbar } from '../components';
 import './Profile.css';
 
 function Profile() {
+  const avatarSrc = 'https://blog.nebrass.fr/wp-content/uploads/Homer-Simpson-4-200x200.jpg';
+
   return (
     <section className="profile-container">
       <Sidebar />
@@ -10,7 +13,29 @@ function Profile() {
         <Topbar />
         <div className="profile-user-container">
           <h2>Seu Perfil</h2>
+          <section className="profile-settings">
+            <div className="avatar-container">
+              <img src={ avatarSrc } alt="Profile avatar" id="profile-avatar" />
+              <Link to="/" className="change-avatar">Trocar avatar</Link>
+            </div>
+            <div className="user-name-container">
+              <h4>Nome de Usuário</h4>
+              <input type="text" className="profile-input" />
+              <button type="button">Alterar Nome</button>
+              <div>
+                <p> Está com algum problema?</p>
+                <Link to="/">Contate o suporte</Link>
+              </div>
+            </div>
+            <div className="password-container">
+              <h4>Senha atual</h4>
+              <input type="text" className="profile-input" />
+              <h4>Nova senha</h4>
+              <input type="text" className="profile-input" />
+              <button type="button">Trocar Senha</button>
 
+            </div>
+          </section>
         </div>
         <Favorites />
         <KeepReading />
