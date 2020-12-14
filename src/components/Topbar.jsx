164c9d21315a32afba9e0b16b710b16ce2a9374b
alcantara2509 <alcantara2509@gmail.com>
2020-12-14 14:08:42 -0300

    topbar icons

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -5,14 +5,20 @@ import './Topbar.css';
 function Topbar() {
   return (
     <section className="topbar-container">
-      <div>
+      <div className="search-div">
         <input type="text" id="search-input" placeholder="Search..." />
-        <button type="button" className="topbar-btn">Search</button>
+        <button type="button" className="topbar-btn">
+          <i className="fas fa-search top-icons" id="search-icon" />
+        </button>
       </div>
       <div>
-        <button type="button" className="topbar-btn">Notify</button>
+        <button type="button" className="topbar-btn set-icons">
+          <i className="far fa-bell top-icons" />
+        </button>
         <Link to="/profile" id="btn-setting">
-          <button type="button" className="topbar-btn">setting</button>
+          <button type="button" className="topbar-btn set-icons">
+            <i className="fas fa-cog top-icons" />
+          </button>
         </Link>
       </div>
     </section>
