8a5633c47ccf1a0738f4821ee9e888002ebd39b6
alcantara2509 <alcantara2509@gmail.com>
2020-12-29 20:42:23 -0300

    slider resize

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -37,48 +37,48 @@ function Slider() {
       style={ {
                 padding: '0',
                 textTransform: 'none',
-      backgroundImage: `url(${tales.strMealThumb})`,
+                backgroundImage: `url(${tales.strMealThumb})`,
     } }
       className="tales-card"
     >
-      <div style={ { height: '100%' } }>
-            <div className="card-infos-container">
-              <div className="card-top">
-                <p className="sup-left-p">Lorem Ipsum</p>
-                <p className="sup-left-p">
-                  <i className="fas fa-clock timer" />
-                  <span style={ { textTransform: 'lowercase' } }>1h ago</span>
-                </p>
-              </div>
-              <div className="card-bottom">
-                <div className="history-container">
-                  <p className="history-title">{tales.title}</p>
-                  <p className="is-favorite">
-                    {
-                      index % 2 !== 0
-                        ? <span
-                            style={ {
-                            fontSize: '14px',
-                            color: 'white',
-                            textTransform: 'uppercase' } }
-                        >
-                          novo
-                          </span>
-                        : <i className="fas fa-heart" />
-                    }
-                  </p>
-                </div>
-                <div className="history-container chapter-container">
-                  <p className="chapter-title">Nome capítulo</p>
-                </div>
-              </div>
+      <div style={ { height: '100%', width: '100%' } }>
+        <div className="card-infos-container">
+          <div className="card-top">
+            <p className="sup-left-p">Lorem Ipsum</p>
+            <p className="sup-left-p">
+              <i className="fas fa-clock timer" />
+              <span style={ { textTransform: 'lowercase' } }>1h ago</span>
+            </p>
+          </div>
+          <div className="card-bottom">
+            <div className="history-container">
+              <p className="history-title">{tales.title}</p>
+              <p className="is-favorite">
+                {
+                  index % 2 !== 0
+                    ? <span
+                        style={ {
+                        fontSize: '14px',
+                        color: 'white',
+                        textTransform: 'uppercase' } }
+                    >
+                      novo
+                      </span>
+                    : <i className="fas fa-heart" />
+                }
+              </p>
             </div>
+            <div className="history-container chapter-container">
+              <p className="chapter-title">Nome capítulo</p>
+            </div>
+          </div>
+        </div>
       </div>
     </Link>
   ));
 
   const breakPoints = [
-    { width: 600, itemsToShow: 1, itemsToScroll: 1 },
+    { width: 600, itemsToShow: 2, itemsToScroll: 1, showArrows: false },
     { width: 601, itemsToShow: 3, itemsToScroll: 2 },
     { width: 850, itemsToShow: 4, itemsToScroll: 2 },
     { width: 1350, itemsToShow: 6, itemsToScroll: 2 },
